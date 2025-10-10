ba187d05f5bd64f1f7908749657d7ab3cb31192d
Vicki-Yu <decemberi7idq@gmail.com>
2025-10-10 21:29:21 +0900

    오류랑 자잘한 거 수정함 / 나가기 아이콘 필요하고 다음에 ㄹㅇ Scene 들어감

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # source/main.py
+"""Scene의 전환을 관리"""
 
 import pygame as py
-from scenes.title_scene import TitleScene
+from source.scenes.title_scene import TitleScene
 
 screen = py.display.set_mode((1920, 1080), py.FULLSCREEN)
 py.display.set_caption("Grow Chick!")
@@ -11,11 +12,11 @@ current_scene = "Title"
 while True:
     if current_scene == "Title":
         scene = TitleScene(screen)
-    
+
     elif current_scene == "Quit":
         break
 
-    next_scene = scene.run()
-    current_scene = next_scene
+    NEXT_SCENE = scene.run()
+    current_scene = NEXT_SCENE
 
 py.quit()
--- source/scenes/title_scene.py
+++ source/scenes/title_scene.py
@@ -1,59 +1,66 @@
 # source/scenes/title_scene.py
+"""타이틀 화면(메인 화면)"""
 
-import pygame as py
 import os
-from util.functions import load_img, show_img
+import pygame as py
+from source.util.functions import load_img, show_img
 
 py.init()
 
 class TitleScene:
+    """타이틀 화면 구성과 기능을 관리하는 클래스"""
 
     def __init__(self, screen):
-        
+
         self.f11 = False
         self.screen = screen
 
         bg_path = os.path.join("assets", "backgrounds", "TitleScreen_bg.png")
         self.bg = load_img(bg_path, scale=(1920, 1080))
 
-        PLAY_path = os.path.join("assets", "ui", "icons", "PLAY_btn.png")
-        self.PLAY = load_img(PLAY_path, scale=(296, 128))
+        play_btn_path = os.path.join("assets", "ui", "icons", "PLAY_btn.png")
+        self.play_btn = load_img(play_btn_path, scale=(296, 128))
+        self.play_btn_rect = None
 
-        QUIT_path = os.path.join("assets", "ui", "icons", "QUIT_btn.png")
-        self.QUIT = load_img(QUIT_path, scale=(296, 128))
+        quit_btn_path = os.path.join("assets", "ui", "icons", "QUIT_btn.png")
+        self.quit_btn = load_img(quit_btn_path, scale=(296, 128))
+        self.quit_btn_rect = None
 
-        Config_path = os.path.join("assets", "ui", "icons", "Config_btn.png")
-        self.Config = load_img(Config_path, scale=(128, 128))
+        config_path = os.path.join("assets", "ui", "icons", "Config_btn.png")
+        self.config_btn = load_img(config_path, scale=(128, 128))
+        self.config_btn_rect = None
 
     def run(self):
-        
+        """객체를 화면에 표시하고 실행 중 기능을 관리"""
+
         running = True
 
         while running:
 
             show_img(self.bg, 0.5, 0.5, self.screen)
-            self.PLAY_rect = show_img(self.PLAY, 0.3, 0.4, self.screen)
-            self.QUIT_rect = show_img(self.QUIT, 0.7, 0.4, self.screen)
-            self.Config_rect = show_img(self.Config, 0.9, 0.1, self.screen)
+            self.play_btn_rect = show_img(self.play_btn, 0.5, 0.75, self.screen)
+            self.quit_btn_rect = show_img(self.quit_btn, 0.1, 0.075, self.screen)
+            self.config_btn_rect = show_img(self.config_btn, 0.95, 0.1, self.screen)
 
             events = py.event.get()
-            
+
             for event in events:
 
                 if event.type == py.QUIT:
                     running = False
 
                 elif event.type == py.KEYDOWN and event.key == py.K_F11:
-                    self.f11 = not f11
+                    self.f11 = not self.f11
 
                     if self.f11:
-                        py.display.set_mode((1900, 1060))
+                        self.screen = py.display.set_mode((1910, 1020))
                     else:
-                        py.display.set_mode((1920, 1080), py.FULLSCREEN)
+                        self.screen = py.display.set_mode((1920, 1080), py.FULLSCREEN)
 
-                elif event.type == py.MOUSEBUTTONDOWN and event.key == self.PLAY_rect.collidepoint(event.pos):
-                    return "Living room"
-        
+                elif event.type == py.MOUSEBUTTONDOWN:
+                    if self.play_btn_rect.collidepoint(event.pos):
+                        return "Living room"
+
             py.display.update()
 
         py.quit()
