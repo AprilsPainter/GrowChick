bb4de26bdc0c8318d9478ded670e30006541415f
Vicki-Yu <decemberi7idq@gmail.com>
2025-10-12 00:47:27 +0900

    모르겟다용

--- source/util/functions.py
+++ source/util/functions.py
@@ -11,19 +11,24 @@ def load_text(text, font_path, size, color, bg_color=None):
     text_surface = font.render(text, True, color, bg_color)
     return text_surface
 
-def show_text(text_surface, width_ratio, height_ratio, screen):
+def show_text(screen, text_surface, w_r=None, h_r=None, x_c=None, y_c=None):
     """텍스트 객체를 표시"""
 
-    screen_width, screen_height = screen.get_size()
-    x = screen_width * width_ratio
-    y = screen_height * height_ratio
-
-    rect = text_surface.get_rect()
-    rect.center = (x, y)
-
-    screen.blit(text_surface, rect)
-
-    return rect
+    if x_c and y_c:
+        rect = text_surface.get_rect()
+        rect.center = (x_c, y_c)
+        screen.blit(text_surface, rect)
+        
+    elif w_r and h_r:
+        w, h = screen.get_size()
+        x = w * w_r
+        y = h * h_r
+        
+        rect = text_surface.get_rect()
+        rect.center = (x, y)
+        screen.blit(text_surface, rect)
+        
+    return rect    
 
 def load_img(img_path, scale=None):
     """이미지 객체를 로드"""
@@ -35,16 +40,21 @@ def load_img(img_path, scale=None):
 
     return image
 
-def show_img(img_surface, width_ratio, height_ratio, screen):
+def show_img(screen, img_surface, w_r=None, h_r=None, x_c=None, y_c=None):
     """이미지 객체를 표시"""
 
-    screen_width, screen_height = screen.get_size()
-    x = screen_width * width_ratio
-    y = screen_height * height_ratio
+    if w_r and h_r:
+        screen_width, screen_height = screen.get_size()
+        x = screen_width * width_ratio
+        y = screen_height * height_ratio
 
-    rect = img_surface.get_rect()
-    rect.center = (x, y)
-
-    screen.blit(img_surface, rect)
-
+        rect = img_surface.get_rect()
+        rect.center = (x, y)
+        screen.blit(img_surface, rect)
+        
+    elif x_c and y_c:
+        rect = img_surface.get_rect()
+        rect.center = (x_c, y_c)
+        screen.blit(img_surface, rect)
+        
     return rect
